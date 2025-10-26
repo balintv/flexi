a69f6744d46e39d68390263bb1c4b310b4a6e7c8
balintv <balintv@gmail.com>
2025-10-26 13:46:41 +0100

    Update flexi_app.py

--- flexi_app.py
+++ flexi_app.py
@@ -742,7 +742,7 @@ else:
                     "ertek": f"{lista_ar_int:,} Ft".replace(",", " "),
                     "reszletezes": f"<b>{legjobb_olcsobb['nev']}</b> + <b>{kulonbozet:,} Ft</b></b><br><b>Megtakarítás:</b> {(lista_ar_int - uj_ar):,} Ft".replace(",", " "),
                     "maradek": " ",
-                    "javaslat": f"A Flexi bérlet {legjobb_olcsobb['nev']} + különbözet opcióval olcsóbb, mint a listaár.",
+                    "javaslat": f"A Flexi + különbözet opcióval olcsóbb, mint a listaár.",
                 }]
         else:
             # eredeti ajánlati logika marad
