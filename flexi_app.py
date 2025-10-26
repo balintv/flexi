237cf9492d72c82347a9cd0b0eda34a9415f0e0b
balintv <balintv@gmail.com>
2025-10-26 12:12:55 +0100

    Update flexi_app.py

--- flexi_app.py
+++ flexi_app.py
@@ -394,7 +394,7 @@ div[data-testid="stNumberInputContainer"] {
 
 
 # --- nézetválasztó ---
-mode = st.radio("Válassz nézetet:", ["💰 Melyik a legjobb bérlet?", "📊 Mi fér a bérletbe?"])
+mode = st.radio("Válassz nézetet:", ["A: Területek + alkalmak → legjobb bérlet", "B: Területek → bérletlehetőségek"])
 
 st.markdown("&nbsp;", unsafe_allow_html=True)
 
@@ -409,7 +409,7 @@ if not paciens_nev:
 st.markdown("&nbsp;", unsafe_allow_html=True)
 
 # ========== "Mi fér a bérletbe? ==========
-if mode == "📊 Mi fér a bérletbe?":
+if mode == "B: Területek → bérletlehetőségek":
     kivalasztott = []
     for meret, teruletek_dict in ARLISTA[nem].items():
         st.markdown(f"##### {meret}")
